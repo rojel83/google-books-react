b73755929d7b216f832f7f71ff8cb29058ae3b3c
Rojel Perez Vez <rojelpv@gmail.com>
2020-05-12 16:28:07 -0400

    exporting and styling compomnents

---
+++ client/src/components/DeleteBtn/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Destructuring the type, className, children and onClick props, applying them to the button element
+function DeleteBtn({ type = "default", className, children, onClick }) {
+    return (
+        <button onClick={onClick} className={["btn btn-lg", `btn-${type}`, className].join(" ")}>
+            {children}
+        </button>
+    );
+}
