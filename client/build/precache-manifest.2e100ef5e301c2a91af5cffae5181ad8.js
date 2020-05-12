a53046be9c11d7c28cfe79c3d65d4263c9f5684c
Rojel Perez Vez <rojelpv@gmail.com>
2020-05-12 00:38:51 -0400

    npm build

---
+++ client/build/precache-manifest.2e100ef5e301c2a91af5cffae5181ad8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a132461c55c3ba5c8c35d48a06a5c14f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77051b22d9cd455f58b9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "99f2c6340343cef7bd1e",
+    "url": "/static/js/2.e943da92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e943da92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77051b22d9cd455f58b9",
+    "url": "/static/js/main.c19e5e9b.chunk.js"
+  },
+  {
+    "revision": "546730ef454e5709235e",
+    "url": "/static/js/runtime-main.751e8272.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
