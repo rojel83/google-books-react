4997e592aab4f8ac326d03d2085ac1d047c215c8
Rojel Perez Vez <rojelpv@gmail.com>
2020-05-12 14:23:56 -0400

    debugging

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -44,7 +44,7 @@ class Results extends Component {
                                                 <h5 className="card-title">{result.title} by {result.authors}</h5>
                                                 <p className="card-text">{result.description}</p>
                                                 <div>
-                                                    <a href={result.link} className="btn badge-pill btn-outline-dark mt-3" target="_blank" >View</a>
+                                                    <a href={result.link} className="btn badge-pill btn-outline-dark mt-3" res="noopener" >View</a>
                                                     <button onClick={() => this.handleSave(result)} className="btn badge-pill btn-outline-warning mt-3 ml-3" >
                                                         {this.state.savedBooks.map(book => book._id).includes(result._id) ? "Unsave" : "Save"}
                                                     </button>
