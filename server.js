0bcf35d2076636d3cf577d41b46c7d3a260dd620
Rojel Perez Vez <rojelpv@gmail.com>
2020-05-13 00:09:21 -0400

    fixing server.js bugs

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
-const express = require("express");
+onst express = require("express");
 const mongoose = require("mongoose");
 const apiRoutes = require("./routes/index");
 const path = require("path");
 const app = express();
-// const PORT = process.env.PORT || 5000;
 const PORT = process.env.PORT || 3001;
-// app.listen(PORT, function () {
-//     console.log("Server running on port 5000...");
-// });
-// http.listen((process.env.PORT || 5000), function () {
-//     console.log('listening on *:5000');
-// });
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets 
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
@@ -23,16 +16,13 @@ if (process.env.NODE_ENV === "production") {
 // Define API routes here
 app.use('/api', apiRoutes);
 
+// Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (_req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useFindAndModify: false
-})
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks")
 
 app.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
